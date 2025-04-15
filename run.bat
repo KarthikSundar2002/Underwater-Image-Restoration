1811479b0cd0982847a605bd43c54c3e5b25b7ca
xaur1en <spatrickpower@gmail..com>
2025-04-15 01:08:30 +0100

    make run.bat work on relative paths except first line

--- run.bat
+++ run.bat
@@ -2,15 +2,15 @@
 cd C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration
 
 
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.00002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.00002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.00002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.00002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
 
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.0002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.0002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.0002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.0002 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
 
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.00001 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.00001 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.00001 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.00001 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
 
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.000005 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
-py C:\\Users\\spatr\\PycharmProjects\\Underwater-Image-Restoration\\main.py -a SpectralTransformer --lr 0.000005 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.000005 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf L1 --optim adamw
+.venv\Scripts\python.exe main.py -a SpectralTransformer --lr 0.000005 --max-epoch 1000 --train-batch-size 1 --test-batch-size 1 -lossf mix --optim adamw
 pause
